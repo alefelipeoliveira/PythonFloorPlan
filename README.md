4c2ea9fd4f80cfd7660a9b136e05a37697a0a23e
alefelipeoliveira <ale.felipe.oliveira@gmail.com>
2020-06-22 14:09:14 -0300

    Edited README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# PythonFloorPlan-" 
+# README
+
+This repository is dedicated to the creation of trajectory history and heat maps in generic floor plans.
+
+## Requirements
+
+Libraries ```matplotib``` and ```pandas``` are necessary.
+
+$ pip install matplotlib pandas
+
+## Run
+
+Running with this command will show the floorplan plot
+```
+$ python main.py
+```
--- main.py
+++ main.py
@@ -9,19 +9,18 @@ img = plt.imread(imgloc)
 #Creating dataframe from trajectory data
 df = pd.read_csv("data\\history.csv")
 
-
 fig, ax = plt.subplots()
 
 #scatter points from trajectory data
 ax.scatter(list(df['x']), list(df['y']))
 
-
+#adapt picture dimensions to floorplan dimension (25x25m) to trajectory points match with floorplan
 ax.imshow(img, extent=[0, 25.00, 0, 25.00])
-plt.grid('off')
 
-#ax.set(xlabel='position x [m]', ylabel='position y [m]', title='Office Plan')
+#turn off grid and ticks
+plt.grid('off')
+plt.axis('off')
 ax.grid()
 
+#show plot
 plt.show()
-
-imgplot = plt.imshow(img)
